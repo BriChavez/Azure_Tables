389bd622e13abcde6d3d2aa9c5dfcad899952541
Brandon Atnip <brandon@theoremadvertising.com>
2022-08-11 10:10:54 -0700

    :construction: Added comments and a few more examples
    Added comments and a few more examples

--- datawrangler/azure/blobs.py
+++ datawrangler/azure/blobs.py
@@ -9,9 +9,31 @@ blobs_app = typer.Typer()
 
 @blobs_app.command('download')
 def blobs_download(filename: str):
+    """
+    Download a single file from an Azure blob container
+    """
     print('Downloading file from Azure blob')
 
 
 @blobs_app.command('upload')
 def blobs_download(filename: str):
+    """
+    Upload a single file to an Azure blob container
+    """
     print('Uploading file to Azure blob')
+
+
+@blobs_app.command('read')
+def blobs_download(filename: str):
+    """
+    Read data from an Azure blob table and save it to a CSV
+    """
+    print('Reading from Azure blob table')
+
+
+@blobs_app.command('write')
+def blobs_download(filename: str):
+    """
+    Write data from a CSV to an Azure blob table
+    """
+    print('Writing to Azure blob table')
